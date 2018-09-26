3cf5a427851bd47fc004c845ae810d2337991280
chris <chris.ayoubtexas@gmail.com>
2018-09-26 17:13:56 -0500

    Update testing script for dd

--- tester.sh
+++ tester.sh
@@ -11,11 +11,11 @@ do
     # Print current size
     echo "Testing input size: $i"
     # Get input file
-    dd if=/dev/urandom of=in_rand bs=1 status=none count=$i
+    dd if=/dev/urandom of=in_rand bs=${i} status=none count=1
     # Get key 128
-    dd if=/dev/urandom of=key_128 bs=1 status=none count=16
+    dd if=/dev/urandom of=key_128 bs=16 status=none count=1
     # Get key 256
-    dd if=/dev/urandom of=key_256 bs=1 status=none count=32
+    dd if=/dev/urandom of=key_256 bs=32 status=none count=1
     # Execute encrypt 128
     $cmd --keysize 128 --keyfile key_128 --inputfile in_rand --outputfile out_enc_128 --mode encrypt
     # Execute encrypt 256
